03729772b11696a794f4dab42b8a74c53f152427
Mark James Aropon <aropon.markjames.august17@gmail.com>
2023-10-06 13:04:33 +0800

    url not working

---
+++ sept-16/Controllers/moviesController.cs
@@ -0,0 +1,37 @@
+﻿using sept_16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sept_16.Controllers
+{
+    public class moviesController : Controller
+    {
+        private moviesMOdel[] movieList = new moviesMOdel[] 
+        {
+            new moviesMOdel{ ID = 16, Title = "titanic" },
+        };
+
+      
+        public ActionResult Details(int ? ID)
+        {
+            moviesMOdel movie = null;
+
+            foreach (var M in movieList)
+            {
+                if (M.ID == ID)
+                {
+                    movie = M;
+                    break;
+
+                }
+            }
+            if (movie == null) {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+    }
+}
---
+++ sept-16/moviesMOdel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sept_16.Models
+{
+    public class moviesMOdel
+    {  
+      public int ID { get; set; }
+      public string Title { get; set; }
+    }
+}
